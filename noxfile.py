c7b401b2d145fb6397a0a00728848c85489ab7f8
Omar AYAD <SESA675734@se.com>
2022-11-16 14:09:28 +0100

    some changes

--- noxfile.py
+++ noxfile.py
@@ -1,1 +1,12 @@
-import noxtree
+import nox
+from typing import List
+
+PYTHON_VERSIONS: List[str] = ["3.9"]
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.error_on_missing_interpreters = True
+
+
+@nox.session()
+def tests(session):
+    session.run("pytest", "tests")
