2530e96aa23bfabaa2045d7a4d771ba4fd9031f6
Omar AYAD <SESA675734@se.com>
2022-11-16 16:59:24 +0100

    some changes

--- src/cli.py
+++ src/cli.py
@@ -33,7 +33,7 @@ hyperparams = {"max_depth": 5,
 
 
 def main():
-    sm_env = os.getenv("ENV", "DEV")
+    sm_env = os.getenv("ENV_NAME", "DEV")
     aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
     aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
     boto_session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key,
